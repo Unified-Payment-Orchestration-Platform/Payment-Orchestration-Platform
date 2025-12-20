c4794ed53b26ea2e9c3e18935a5be3a2114a3668
Saad Musema Yusuf <saadmusema3@gmail.com>
2025-12-20 10:11:13 +0300

    refactor/api: remove unnecessary controllers

--- services/api-gateway-service/src/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const PaymentController = require('../controllers/paymentController');
-
-router.post('/', (req, res) => PaymentController.initiatePayment(req, res));
-router.get('/:id', (req, res) => PaymentController.getPaymentStatus(req, res));
-
-module.exports = router;
